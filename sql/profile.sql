09cd660aa3645d5cdd6f60553087ed305b090646
Jeffsmov <miguelbonillac.24@gmail.com>
2021-06-05 05:23:35 +0000

    profile detail

--- sql/profile.sql
+++ sql/profile.sql
@@ -10,10 +10,10 @@
 }
 */
 --                                                                      sql         | endpoint
--- get profile                                                          not done    | not done
--- get profiles                                                         not done    | not done
--- insert profile                                                       not done    | not done
--- update profile                                                       not done    | not done
+-- get profile                                                          done        | not done
+-- get profiles                                                         done        | not done
+-- insert profile                                                       done (mike) | not done
+-- update profile                                                       done        | not done
 
 DELIMITER //
 
@@ -32,7 +32,31 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS spGetProfiles;
 CREATE PROCEDURE spGetProfiles()
 BEGIN    
-	select * from tbProfile where inId = `id`;
+	select * from tbProfile;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS spUpdateTbProfile;
+CREATE PROCEDURE spUpdateTbProfile(   
+    IN inId integer,
+    IN inUserId integer,
+    IN inName VARCHAR(50),
+    IN inDescription VARCHAR(250),
+    IN inDayrange integer,
+    IN inStartday date,
+    IN inColor VARCHAR(50)
+) 
+BEGIN    
+	UPDATE tbProfile
+	SET `name` = inName,
+		`description` = inDescription,
+		`dayrange` = inDayrange,
+		`startday` = inStartday,
+		`color` = inColor
+	WHERE `id` = inId and `iduserdata` = inUserId;
 END //
 
 DELIMITER ;
